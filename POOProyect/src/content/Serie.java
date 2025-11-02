1ae167602bcd779576f05e1fbe9276ff0c619cb3
user <user@192.168.1.132>
2025-11-02 08:48:39 -0500

    hoa :D

--- POOProyect/src/content/Serie.java
+++ POOProyect/src/content/Serie.java
@@ -1,6 +1,57 @@
 
 package content;
 
-public class Serie {
-    
+public class Serie extends AudioVisualContent{
+   private int seasons;
+   private int chapters;
+   private double averageChapterDuration;
+   
+public Serie(){
+    this.seasons = 0;
+    this.chapters = 0;
+    this.averageChapterDuration = 0;
+   }
+public Serie(int seasons, int chapters, double averageChapterDuration){
+    this.setSeasons(seasons);
+    this.setChapters(chapters);
+    this.setAverageChapterDuration(averageChapterDuration);
+}
+public final void setSeasons(int seasons){
+    this.seasons = seasons;
+}
+public final void setChapters(int chapters){
+    this.chapters = chapters;
+}
+public final void setAverageChapterDuration(double averageChapterDuration){
+    this.averageChapterDuration = averageChapterDuration;
+}
+public int getSeasons(){
+    return this.seasons;
+}
+public int getChapters(){
+    return this.chapters;
+}
+public double getAverageChapterDuration(){
+    return this.averageChapterDuration;
+}
+
+@Override
+public String toString(){
+    String str = super.toString() + "\nNumber of seasons: " + this.seasons + "\nNumber of chapters: " + this.chapters + 
+                "\nAverage of chapter duration: " + this.averageChapterDuration;
+    return str;
+}
+@Override
+public double CalculateTotalDuration(){
+    double a = this.averageChapterDuration * this.chapters;
+    return a;
+}
+@Override
+public double CalculateProductionBudget(){
+    double b = 0;
+    return b;
+}
+
+
+
 }
