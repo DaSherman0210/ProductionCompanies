2b2543acadcaa65b71185d02c85af2ebeee490f6
user <user@192.168.1.132>
2025-11-03 12:50:33 -0500

    issue14

--- POOProyect/src/content/StopMotionSeries.java
+++ POOProyect/src/content/StopMotionSeries.java
@@ -1,6 +1,53 @@
 
 package content;
 
-public class StopMotionSeries {
+import staff.Director;
+
+public class StopMotionSeries extends Serie{
+    private int fps;
+    private String technique;
+    private String dimension;
+    
+public StopMotionSeries(){
+    this.fps = 0;
+    this.technique = null;
+    this.dimension = null;
+}
+public StopMotionSeries(String id, String title, String synopsis, String releaseDate, String genre, Director director, Double rating, String originalLanguage, int seasons, int chapters, double averageChapterDuration, int fps, String technique, String dimension){
+    super(id, title, synopsis, releaseDate, genre, director, rating, originalLanguage, seasons, chapters, 0);
+    
+    
+    
+}    
+public final void setFps(int fps){
+    this.fps = fps;
+}    
+public final void setTechnique(String technique){
+    this.technique = technique;
+}    
+public final void setDimensio(String dimension){
+    this.dimension = technique;
+}    
+public int getFps(){
+    return this.fps;
+}    
+public String getTechnique(){
+    return this.technique;
+}    
+public String getDimension(){
+    return this.dimension;
+}    
+    
+@Override
+public String toString(){
+   String str = super.toString() + "\nNumber of fps: " + this.fps + "\nUsed technique: " + this.technique + "\nDimension: " + this.dimension;
+    return str;
+}
+    
+    
+    
+    
+    
+    
     
 }
