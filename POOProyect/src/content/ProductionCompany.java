b1587b641939fb69444f79ab6aa406e2da31b44c
CENTIC <CENTIC@10.6.65.224>
2025-11-04 10:38:36 -0500

    :feat: Finished production company file.

--- POOProyect/src/content/ProductionCompany.java
+++ POOProyect/src/content/ProductionCompany.java
@@ -1,48 +1,56 @@
 
 package content;
 
+import pooproyect.Validation;
 import java.util.ArrayList;
 
 public class ProductionCompany {
     
-    private int id;
+    private String id;  
     private String name;
     private String country;
     private String foundationDate;
     private ArrayList<AudiovisualContent> audiovisualContent;
     
     public ProductionCompany(){
-        this.id = 0;
+        this.id = "";
         this.name = "";
         this.country = "";
         this.foundationDate = "";
     }
     
-    public ProductionCompany(int id, String name, String country, String foundationDate){
-        
+    public ProductionCompany(int String, String name, String country, String foundationDate){
+        this.setId(id);
+        this.setName(name);
+        this.setCountry(country);
+        this.setFoundationDate(foundationDate);
     }
     
     // SETS
     
-    public final void setId(int id){
-        
+    public final void setId(String id){
+        Validation.validateTextNumbers(id);
+        this.id = id;
     }
     
     public final void setName(String name){
-        
+        Validation.validateText(name);
+        this.name = name;
     }
     
     public final void setCountry(String country){
-        
+        Validation.validateText(country);
+        this.country = country;
     }
     
     public final void setFoundationDate(String foundationDate){
-        
+        // MISSING VALIDATION
+        this.foundationDate = foundationDate;
     }
     
     // GETS
     
-    public int getId(){
+    public String getId(){
         return this.id;
     }
     
@@ -57,5 +65,20 @@ public class ProductionCompany {
     public String getFoundationDate(){
         return this.foundationDate;
     }
-       
+    
+    // OTHERS
+    
+    public void addAudioVisualContent(AudiovisualContent audiovisualContent){
+        this.audiovisualContent.add(audiovisualContent);
+    }
+    
+    // OVERRIDE
+    
+    @Override
+    
+    public String toString(){
+        String res = "Id: " + this.getId() + "\n Name: " + this.getName() + "\n Country: " +  this.getCountry() + "\n Foundation Date: " + this.getFoundationDate(); 
+        return res;
+    }
+    
 }
