555f64b63db8d2046fd15a1af25244cf05df5cc1
CENTIC <CENTIC@10.6.65.236>
2025-11-07 11:43:17 -0500

    issue7

--- POOProyect/src/content/LiveActionSeries.java
+++ POOProyect/src/content/LiveActionSeries.java
@@ -12,8 +12,8 @@ public LiveActionSeries(){
     this.scriptSupervisor = true;
     this.locations = new ArrayList<>();
 }    
-public LiveActionSeries(int seasons, int chapters, double averageChapterDuration, String id, String title, String synopsis, String releaseDate, String genre, Director director, Double rating, String originalLanguage, boolean scriptSupervisor){
-super(seasons, chapters, averageChapterDuration, id, title, synopsis, releaseDate, genre, director, rating, originalLanguage);
+public LiveActionSeries(String id, String title, String synopsis, String releaseDate, String genre, Director director, Double rating, String originalLanguage, int seasons, int chapters, double averageChapterDuration, boolean scriptSupervisor){
+super(id, title, synopsis, releaseDate, genre, director, rating, originalLanguage, seasons, chapters, averageChapterDuration);
  this.setScriptSupervisor(scriptSupervisor);
 }
 public final void setScriptSupervisor(boolean scriptSupervisor){
@@ -34,5 +34,11 @@ public String toString(){
     }
     return str;
 }    
+
+    @Override
+    public double calculateProductionBadget() {
+        double a = 0;
+        return a;
+    }
     
 }    
