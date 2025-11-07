740a6941208f49bca554f6893e513dbd4db0b84b
CENTIC <CENTIC@10.6.65.236>
2025-11-07 11:43:27 -0500

    issue7

--- POOProyect/src/content/Serie.java
+++ POOProyect/src/content/Serie.java
@@ -3,7 +3,7 @@ package content;
 
 import staff.Director;
 
-public class Serie extends AudiovisualContent{
+public abstract class Serie extends AudiovisualContent{
    private int seasons;
    private int chapters;
    private double averageChapterDuration;
@@ -13,7 +13,7 @@ public Serie(){
     this.chapters = 0;
     this.averageChapterDuration = 0;
    }
-public Serie(int seasons, int chapters, double averageChapterDuration, String id, String title, String synopsis, String releaseDate, String genre, Director director, Double rating, String originalLanguage){
+public Serie(String id, String title, String synopsis, String releaseDate, String genre, Director director, Double rating, String originalLanguage, int seasons, int chapters, double averageChapterDuration){
     super(id, title, synopsis, releaseDate, genre, director, rating, originalLanguage);
     this.setSeasons(seasons);
     this.setChapters(chapters);
@@ -50,13 +50,5 @@ public String toString(){
         double a = 0;
     return a;
     }
-
-    @Override
-    public double calculateProductionBadget() {
-        double b = 0;
-    return b;
-    }
-
-
 
 }
