82d15e17abb857570a467ee1351db57538d1bcc8
CENTIC <CENTIC@10.6.65.152>
2025-11-07 12:48:39 -0500

    en progreso

--- POOProyect/src/content/LiveActionMovies.java
+++ POOProyect/src/content/LiveActionMovies.java
@@ -1,6 +1,27 @@
 
 package content;
 
-public class LiveActionMovies {
+import java.util.ArrayList;
+
+public class LiveActionMovies extends Movie{
+    //Attrubites 
+    private boolean scriptSupervisor;
+    private ArrayList<String> locations;
     
+    //Constructors
+    public LiveActionMovies (){
+    this.scriptSupervisor = false;
+    this.locations = new ArrayList<String>();
+    }
+    
+    //Set and Gets
+    
+    
+    //Unique methods 
+    
+    
+    //toString
+    
+    
+    //Abstract method overwritten
 }
