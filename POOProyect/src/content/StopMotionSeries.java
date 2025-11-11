9cd1f3c5b24f01c12fade08d5b1ce5485328a606
user <user@DESKTOP-4HVRD20>
2025-11-11 11:52:12 -0500

    issue14

--- POOProyect/src/content/StopMotionSeries.java
+++ POOProyect/src/content/StopMotionSeries.java
@@ -50,7 +50,7 @@ public String toString(){
 }
 
     @Override
-    public double calculateProductionBadget() {
+    public double calculateProductionBudget() {
       double a =0;
       return a;
     }
