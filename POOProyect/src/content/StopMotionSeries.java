2763d9d0e2643f43c84a413e2aabb08787f60b30
user <user@DESKTOP-4HVRD20>
2025-11-11 11:49:24 -0500

    issue31

--- POOProyect/src/content/StopMotionSeries.java
+++ POOProyect/src/content/StopMotionSeries.java
@@ -1,6 +1,64 @@
 
 package content;
 
-public class StopMotionSeries {
+import pooproyect.Validation;
+import staff.Director;
+
+public class StopMotionSeries extends Serie{
+    private int fps;
+    private String technique;
+    private String dimension;
+    
+public StopMotionSeries(){
+    this.fps = 0;
+    this.technique = null;
+    this.dimension = null;
+}
+public StopMotionSeries(String id, String title, String synopsis, String releaseDate, String genre, Director director, Double rating, String originalLanguage, int seasons, int chapters, double averageChapterDuration, int fps, String technique, String dimension){
+    super(id, title, synopsis, releaseDate, genre, director, rating, originalLanguage, seasons, chapters, averageChapterDuration);
+    this.setFps(fps);
+    this.setDimension(dimension);
+    this.setTechnique(technique);
+}    
+public final void setFps(int fps){
+    Validation.validatePositiveNumber(fps);
+    this.fps = fps;
+}    
+public final void setTechnique(String technique){
+    Validation.validateText(technique);
+    this.technique = technique;
+}    
+public final void setDimension(String dimension){
+    Validation.validateText(technique);
+    this.dimension = technique;
+}    
+public int getFps(){
+    return this.fps;
+}    
+public String getTechnique(){
+    return this.technique;
+}    
+public String getDimension(){
+    return this.dimension;
+}    
+    
+@Override
+public String toString(){
+   String str = super.toString() + "\nNumber of fps: " + this.fps + "\nUsed technique: " + this.technique + "\nDimension: " + this.dimension;
+    return str;
+}
+
+    @Override
+    public double calculateProductionBudget() {
+      double a =0;
+      return a;
+    }
+    
+    
+    
+    
+    
+    
     
 }
+
